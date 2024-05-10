b1c7a17f572668ac5d96da3fe0bd95769d17c63f
Pere <pere.pachi9@gmail.com>
2024-05-10 14:46:49 +0200

    git init

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'expirience', component: ExperienciaComponent },
+    { path: 'tecnologies', component: TecnologiasComponent },
+    { path: 'projects', component: ProjectsComponent }
+];
